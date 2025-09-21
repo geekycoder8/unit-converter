6bc968ceeb9612b8f2a7c2881c8a290c50088529
Mohamed Faisal <142818865+geekycoder8@users.noreply.github.com>
2025-09-21 15:36:44 +0400

    update index.js

--- index.js
+++ index.js
@@ -16,27 +16,26 @@ convertBtn.addEventListener("click", function () {
 })
 
 function lengthConverter(valNum) {
-	const feetToMeters = parseFloat(valNum / 3.2808).toFixed(3)
-	const metersToFeet = parseFloat(valNum * 3.2808).toFixed(3)
+	const feetToMeters = parseFloat((valNum / 3.2808).toFixed(3))
+	const metersToFeet = parseFloat((valNum * 3.2808).toFixed(3))
 	paragraphEl[0].textContent = `${valNum} meters = ${metersToFeet} feet | ${valNum} feet = ${feetToMeters} meters`
 }
 
 
 function volumeConverter(valNum) {
-	const litersToGallons = parseFloat(valNum * 0.264172).toFixed(3)
-	const gallonsToLiters = parseFloat(valNum * 3.78541).toFixed(3)
+	const litersToGallons = parseFloat((valNum * 0.264172).toFixed(3))
+	const gallonsToLiters = parseFloat((valNum * 3.78541).toFixed(3))
 	paragraphEl[1].textContent = `${valNum} liters = ${litersToGallons} gallons | ${valNum} gallons = ${gallonsToLiters} liters`
 }
 
 
 function massConverter(valNum) {
-	const kilosToPounds = parseFloat(valNum * 2.20462).toFixed(3)
-	const poundsToKilos = parseFloat(valNum * 0.453592).toFixed(3)
+	const kilosToPounds = parseFloat((valNum * 2.20462).toFixed(3))
+	const poundsToKilos = parseFloat((valNum * 0.453592).toFixed(3))
 	paragraphEl[2].textContent = `${valNum} kilos = ${kilosToPounds} pounds | ${valNum} pounds = ${poundsToKilos} kilos`
 }
 
 function inputValue(value) {
-	console.log(value, "!!")
 	lengthConverter(value)
 	volumeConverter(value)
 	massConverter(value)
